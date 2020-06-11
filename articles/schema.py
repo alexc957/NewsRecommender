194d96eed5fc032a961514e6fe7686db6524c0e0
alexc957 <alexcoronel1995@gmail.com>
2020-06-11 13:07:11 -0500

    spacy model deleted

--- articles/schema.py
+++ articles/schema.py
@@ -1,57 +1,53 @@
-import graphene 
+import math
+import graphene
 from graphene_django import DjangoObjectType
 
 from .models import Article, SimilarArticle
 from users.schema import UserType
 from django.db.models import Count
-import spacy
+#import spacy
 from django.db.models import Q
-#from recommendations.Recommender import Recommender 
-nlp = spacy.load('es_core_news_md')
-import math
+#from recommendations.Recommender import Recommender
+#nlp = spacy.load('es_core_news_md')
 
+#recommender = Recommender(threshold=0.95)
 
-
-#recommender = Recommender(threshold=0.95) 
 
 class ArticleType(DjangoObjectType):
-  
+
     class Meta:
         model = Article
+
 
 class SimilarArticleType(DjangoObjectType):
     class Meta:
         model = SimilarArticle
-    
+
+
 class Query(graphene.ObjectType):
     articles = graphene.List(
-        ArticleType, 
-        search = graphene.String(),
-        first = graphene.Int(), 
-        skip = graphene.Int())
+        ArticleType,
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int())
 
     article = graphene.Field(
         ArticleType,
-        article_id = graphene.Int(required = True)
+        article_id=graphene.Int(required=True)
     )
 
     recent_articles = graphene.List(ArticleType)
 
     most_voted = graphene.List(ArticleType)
 
-    total_pages = graphene.Int() 
+    total_pages = graphene.Int()
 
-    def resolve_total_pages(self,info,**kwargs):
-        all_articles = Article.objects.all() 
+    def resolve_total_pages(self, info, **kwargs):
+        all_articles = Article.objects.all()
         total_pages = math.ceil(len(all_articles)/10)
         return total_pages
 
-    
-
-    
-
-
-    def resolve_articles(self, info,search=None,first = None, skip= None , **kwargs):
+    def resolve_articles(self, info, search=None, first=None, skip=None, **kwargs):
         qs = Article.objects.all()
         if search:
             filter = (
@@ -65,25 +61,26 @@ class Query(graphene.ObjectType):
         if skip:
             qs = qs[skip:]
         if first:
-            qs = qs[:first]   
-        return qs 
-    def resolve_article(self, info, article_id = None, **kwargs):
-        article = Article.objects.get(id = article_id)
+            qs = qs[:first]
+        return qs
+
+    def resolve_article(self, info, article_id=None, **kwargs):
+        article = Article.objects.get(id=article_id)
 
         if not article:
             raise Exception('Bad article Id')
-        #if not article:
-        #    recommender.generate_recommendations_based_on_one_article(article)    
+        # if not article:
+        #    recommender.generate_recommendations_based_on_one_article(article)
         return article
 
-    def resolve_recent_articles(self,info,**kwargs):
+    def resolve_recent_articles(self, info, **kwargs):
         print('hello')
-        articles =  Article.objects.all().order_by('-date_uploaded')
+        articles = Article.objects.all().order_by('-date_uploaded')
         return articles[:10]
 
-    def resolve_most_voted(self,info, **kwargs):
+    def resolve_most_voted(self, info, **kwargs):
         votes = Article.objects.annotate(num_votes=Count('vote'))
-        return votes.order_by('-num_votes')[:10] 
+        return votes.order_by('-num_votes')[:10]
 
 
 class AddArticle(graphene.Mutation):
@@ -94,43 +91,42 @@ class AddArticle(graphene.Mutation):
     category = graphene.String()
     date_uploaded = graphene.Date()
     text_vector = graphene.String()
-    
 
     class Arguments:
         title = graphene.String()
         summary = graphene.String()
         lang = graphene.String()
-        category = graphene.String()       
+        category = graphene.String()
         text_vector = graphene.String()
-    
-    def mutate(self, info, title, summary, lang, category,text_vector=None):
-        if not text_vector:
-            text_vector = ';'.join(nlp(summary).vector.astype(str))
+
+    def mutate(self, info, title, summary, lang, category, text_vector=None):
+        # if not text_vector:
+        #text_vector = ';'.join(nlp(summary).vector.astype(str))
 
         article = Article(
-            title = title,
-            summary = summary,
-            lang = lang,
-            category = category,
-            text_vector = text_vector
+            title=title,
+            summary=summary,
+            lang=lang,
+            category=category,
+            text_vector=text_vector
         )
 
         article.save()
 
         return AddArticle(
-            id = article.id,
-            title = article.title,
-            summary = article.summary,
-            lang = article.lang,
-            category = article.category,
-            date_uploaded = article.date_uploaded,
-            text_vector = article.text_vector
+            id=article.id,
+            title=article.title,
+            summary=article.summary,
+            lang=article.lang,
+            category=article.category,
+            date_uploaded=article.date_uploaded,
+            text_vector=article.text_vector
 
 
         )
 
 
-#class CreateVote(graphene.Mutation):
+# class CreateVote(graphene.Mutation):
  #   user = graphene.Field(UserType)
   #  article = graphene.Field(ArticleType)
 
@@ -145,13 +141,12 @@ class AddArticle(graphene.Mutation):
        # if not article:
        #     raise Exception('Invalid Article')
 
-        #Vote.objects.create(
+        # Vote.objects.create(
         #    user = user,
         #    article = article,
-        #)
+        # )
 
-        #return CreateVote(user= user, article = article)
-
+        # return CreateVote(user= user, article = article)
 
 
 class Mutation(graphene.ObjectType):
